92f64525d2d4fb38287f4674bdf5ba505e15eb0c
Levar Norwood <46905650+lev2pr0@users.noreply.github.com>
2025-04-11 21:55:58 -0400

    Update publicDLreport.ps1

--- publicDLreport.ps1
+++ publicDLreport.ps1
@@ -20,7 +20,7 @@ Function publicDLreport {
             }
         } catch {
             Write-Host "Error connecting to Exchange Online: $_" -ForegroundColor Red
-            Write-Host "If using on-premises Exchange, then rerun use -onPremEX switch" -ForegroundColor Red
+            Write-Host "If using on-premise Exchange, then rerun use -onPremEX switch" -ForegroundColor Red
             return
         }
     } else {
