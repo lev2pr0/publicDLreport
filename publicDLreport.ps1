dc1ff508a99e11a2d9b7f68064dd786c16c15827
Levar Norwood <46905650+lev2pr0@users.noreply.github.com>
2025-04-13 08:50:09 -0400

    Update publicDLreport.ps1
    Remove test path logic

--- publicDLreport.ps1
+++ publicDLreport.ps1
@@ -86,12 +86,6 @@ Function publicDLreport {
             }
     }
 
-    # Ensure output directory exists
-    $directory = Split-Path -Path $OutputPath
-    if (!(Test-Path -Path $directory)) {
-        New-Item -ItemType Directory -Path $directory -Force
-    }
-
     # Export results to CSV
     try {
         $results | Export-Csv -Path $OutputPath -NoTypeInformation
